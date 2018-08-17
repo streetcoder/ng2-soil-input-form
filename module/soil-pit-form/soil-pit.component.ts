3173dafe977ea9331a79def4a01b88eaad72e197
streetcoder <akhirul@nascenia.com>
2018-08-17 11:52:16 -0600

    validated length

--- module/soil-pit-form/soil-pit.component.ts
+++ module/soil-pit-form/soil-pit.component.ts
@@ -84,7 +84,7 @@ import {SurfexMod} from "./soil-data-defs/surfex-mod.enum";
               <!--soil classification-->
               <div class="form-group col-xs-12 col-md-6">
                   <label>Soil Classification (s_grp.g_grp)</label>
-                  <input type="text" class="form-control" formControlName="soilClassification">
+                  <input type="text" class="form-control" maxlength="10" formControlName="soilClassification">
               </div>
 
               <div class="form-group col-xs-12 col-md-6">
@@ -284,7 +284,7 @@ export class SoilPitFormComponent implements OnInit {
         landuse: '',
         stoniness: '',
         soilClassification:'',
-        edition: ''
+        edition: 3
     });
 
     this.addHorizon();
